33eca60fe57a9366d9560e95cca7dd12176d2380
kea <bench@krugerengelbrecht.co.za>
2019-08-07 10:40:42 +0000

    Remove Court report and change fields afet recreate

--- lit_management/lit_management/doctype/litigation_court_report/litigation_court_report.js
+++ lit_management/lit_management/doctype/litigation_court_report/litigation_court_report.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2019, KEA and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('LOC COURT REPORTS', {
-	refresh: function(frm) {
+frappe.ui.form.on('Litigation Court Report', {
+	// refresh: function(frm) {
 
-	}
+	// }
 });
--- lit_management/lit_management/doctype/litigation_court_report/test_litigation_court_report.py
+++ lit_management/lit_management/doctype/litigation_court_report/test_litigation_court_report.py
@@ -3,8 +3,8 @@
 # See license.txt
 from __future__ import unicode_literals
 
-import frappe
+# import frappe
 import unittest
 
-class TestLOCCOURTREPORTS(unittest.TestCase):
+class TestLitigationCourtReport(unittest.TestCase):
 	pass
